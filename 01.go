440ef9744523430bf928f3c0166c3f8134c5164e
Mike <mcornell74@gmail.com>
2021-12-01 21:41:26 -0500

    initial

---
+++ 01.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ParseInput(input string) []string {
+	return strings.Split(input, "\n")
+}
+
+func main() {
+	fmt.Println("ho ho ho")
+
+}
---
+++ 01_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var test_input_01 = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`
+
+func TestParseInput(t *testing.T) {
+
+	parsed := ParseInput(test_input_01)
+	assert.Equal(t, 10, len(parsed))
+	assert.Equal(t, "260", parsed[8])
+}
