4718374593de7c9581c70233442bb1c7346a64fe
Mike <mcornell74@gmail.com>
2021-12-05 11:23:07 -0500

    I do not like the checkers but they work

--- advent04/04.go
+++ advent04/04.go
@@ -1,7 +1,6 @@
 package advent04
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -10,24 +9,76 @@ type Card struct {
 	Numbers [][]int
 }
 
+func (c *Card) MarkNumber(number int) {
+	for row_idx, row := range c.Numbers {
+		for col_idx, col := range row {
+			if col == number {
+				c.Numbers[row_idx][col_idx] = -1
+			}
+		}
+	}
+}
+
+func (c *Card) CheckWinner() bool {
+	if c.CheckRows() {
+		return true
+	}
+	return c.CheckColumns()
+}
+
+func (c *Card) CheckRows() bool {
+	retVal := false
+	for _, row := range c.Numbers {
+		for _, col := range row {
+			retVal = true
+			if col > -1 {
+				retVal = false
+				break
+			}
+		}
+		if retVal {
+			break
+		}
+	}
+	return retVal
+}
+
+func (c *Card) CheckColumns() bool {
+	retVal := false
+	column_count := len(c.Numbers[0])
+	for col := 0; col < column_count; col++ {
+		for _, row := range c.Numbers {
+			retVal = true
+			if row[col] > -1 {
+				retVal = false
+				break
+			}
+		}
+		if retVal {
+			break
+		}
+	}
+	return retVal
+}
+
 func NewCard(lines []string, lineIndex int) *Card {
 	cardPointer := new(Card)
 
-	fmt.Printf("Lines Index: %#v\n", lines[lineIndex:lineIndex+5])
+	// fmt.Printf("Lines Index: %#v\n", lines[lineIndex:lineIndex+5])
 
-	for idx, line := range lines[lineIndex : lineIndex+5] {
-		fmt.Printf("Line %d: %v\n", idx, line)
+	for _, line := range lines[lineIndex : lineIndex+5] {
+		// fmt.Printf("Line %d: %v\n", idx, line)
 		numStringArray := strings.Fields(line)
-		fmt.Printf("NumStringSlice %d: %v\n", idx, numStringArray)
+		// fmt.Printf("NumStringSlice %d: %v\n", idx, numStringArray)
 		var numArray []int
 		for _, num := range numStringArray {
 			val, _ := strconv.Atoi(num)
 			numArray = append(numArray, val)
 		}
-		fmt.Printf("Num Array: %#v\n", numArray)
+		// fmt.Printf("Num Array: %#v\n", numArray)
 		cardPointer.Numbers = append(cardPointer.Numbers, numArray)
 	}
-	fmt.Printf("Numbers: %v\n", cardPointer.Numbers)
+	// fmt.Printf("Numbers: %v\n", cardPointer.Numbers)
 	return cardPointer
 }
 
@@ -38,13 +89,21 @@ func ParseInput(report []string) ([]int, []Card) {
 		val, _ := strconv.Atoi(num)
 		numbers = append(numbers, val)
 	}
-	println("cards")
+	// println("cards")
 	cards := []Card{}
 	for lineNum := 2; lineNum < len(report); lineNum += 6 {
-		fmt.Printf("line num: %v\n", lineNum)
+		// fmt.Printf("line num: %v\n", lineNum)
 		cards = append(cards, *NewCard(report, lineNum))
-		fmt.Printf("Card! %v\n\n", cards[len(cards)-1])
+		// fmt.Printf("Card! %v\n\n", cards[len(cards)-1])
 	}
-	println("cards!")
+	// println("cards!")
 	return numbers, cards
+}
+
+func DrawNumber(numbers []int, cards []Card) ([]int, []Card) {
+	num := numbers[0]
+	for _, card := range cards {
+		card.MarkNumber(num)
+	}
+	return numbers[1:], cards
 }
