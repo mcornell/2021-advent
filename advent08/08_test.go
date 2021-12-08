82bfe657d0dcc61bfe03f71a3ba26cde93e41b04
Mike Cornell <mcornell74@gmail.com>
2021-12-08 13:36:20 -0500

    standing up day 8

---
+++ advent08/08_test.go
@@ -0,0 +1,12 @@
+package advent08
+
+/*
+Each entry consists of ten unique signal patterns, a | delimiter,
+and finally the four digit output value. Within an entry,
+the same wire/segment connections are used (but you don't know what the connections actually are).
+The unique signal patterns correspond to the ten different ways the submarine
+tries to render a digit using the current wire/segment connections.
+Because 7 is the only digit that uses three segments, dab in the above example means
+that to render a 7, signal lines d, a, and b are on. Because 4 is the only digit that
+ uses four segments, eafb means that to render a 4, signal lines e, a, f, and b are on.
+*/
